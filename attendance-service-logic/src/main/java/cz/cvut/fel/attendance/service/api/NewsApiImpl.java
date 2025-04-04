195cbd312d35861ed011b92120530aeb63f8c599
Duc Thang Le <leductha@fel.cvut.cz>
2025-04-04 20:50:06 +0200

    🚧Parent reverted, news commented

--- attendance-service-logic/src/main/java/cz/cvut/fel/attendance/service/api/NewsApiImpl.java
+++ attendance-service-logic/src/main/java/cz/cvut/fel/attendance/service/api/NewsApiImpl.java
@@ -1,39 +1,39 @@
-package cz.cvut.fel.attendance.service.api;
-
-import cz.cvut.fel.attendance.service.service.NewsService;
-import cz.fel.cvut.attendance.service.api.NewsApi;
-import cz.fel.cvut.attendance.service.model.NewsDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-public class NewsApiImpl implements NewsApi {
-
-    private final NewsService newsService;
-
-    @Override
-    public ResponseEntity<NewsDto> createNews(NewsDto newsDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(newsService.createNews(newsDto));
-    }
-
-    @Override
-    public ResponseEntity<List<NewsDto>> getNews() {
-        return ResponseEntity.ok(newsService.getNews());
-    }
-
-    @Override
-    public ResponseEntity<NewsDto> updateNews(Long id, NewsDto newsDto) throws Exception {
-        return ResponseEntity.ok(newsService.updateNews(id,newsDto));
-    }
-
-    @Override
-    public ResponseEntity<Void> deleteNews(Long id) throws Exception {
-        newsService.deleteNews(id);
-        return ResponseEntity.noContent().build();
-    }
-}
+//package cz.cvut.fel.attendance.service.api;
+//
+//import cz.cvut.fel.attendance.service.service.NewsService;
+//import cz.fel.cvut.attendance.service.api.NewsApi;
+//import cz.fel.cvut.attendance.service.model.NewsDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class NewsApiImpl implements NewsApi {
+//
+//    private final NewsService newsService;
+//
+//    @Override
+//    public ResponseEntity<NewsDto> createNews(NewsDto newsDto) {
+//        return ResponseEntity.status(HttpStatus.CREATED).body(newsService.createNews(newsDto));
+//    }
+//
+//    @Override
+//    public ResponseEntity<List<NewsDto>> getNews() {
+//        return ResponseEntity.ok(newsService.getNews());
+//    }
+//
+//    @Override
+//    public ResponseEntity<NewsDto> updateNews(Long id, NewsDto newsDto) throws Exception {
+//        return ResponseEntity.ok(newsService.updateNews(id,newsDto));
+//    }
+//
+//    @Override
+//    public ResponseEntity<Void> deleteNews(Long id) throws Exception {
+//        newsService.deleteNews(id);
+//        return ResponseEntity.noContent().build();
+//    }
+//}
--- attendance-service-logic/src/main/java/cz/cvut/fel/attendance/service/service/NewsService.java
+++ attendance-service-logic/src/main/java/cz/cvut/fel/attendance/service/service/NewsService.java
@@ -1,49 +1,49 @@
-package cz.cvut.fel.attendance.service.service;
-
-import cz.cvut.fel.attendance.service.mappers.NewsMapper;
-import cz.cvut.fel.attendance.service.model.News;
-import cz.cvut.fel.attendance.service.repository.NewsRepository;
-import cz.fel.cvut.attendance.service.model.NewsDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class NewsService {
-
-    private final NewsMapper newsMapper;
-
-    private final NewsRepository newsRepository;
-
-    public NewsDto createNews(NewsDto newsDto) {
-        News news = newsMapper.toEntity(newsDto);
-
-        newsRepository.save(news);
-        return newsMapper.toDto(news);
-    }
-
-    public List<NewsDto> getNews() {
-        return newsMapper.toDtoList(newsRepository.findAll());
-    }
-
-    public NewsDto updateNews(Long id, NewsDto newsDto) throws Exception {
-        News news = newsRepository.findById(id)
-                .orElseThrow(() -> new Exception("News with ID " + id + " not found."));
-
-        newsMapper.updateNewsFromDto(newsDto, news);
-
-        newsRepository.save(news);
-        return newsMapper.toDto(news);
-    }
-
-    public void deleteNews(Long id) throws Exception {
-        News news = newsRepository.findById(id)
-                .orElseThrow(() -> new Exception("News with ID " + id + " not found."));
-        newsRepository.delete(news);
-    }
-}
+//package cz.cvut.fel.attendance.service.service;
+//
+//import cz.cvut.fel.attendance.service.mappers.NewsMapper;
+//import cz.cvut.fel.attendance.service.model.News;
+//import cz.cvut.fel.attendance.service.repository.NewsRepository;
+//import cz.fel.cvut.attendance.service.model.NewsDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.cache.annotation.CacheEvict;
+//import org.springframework.cache.annotation.CachePut;
+//import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class NewsService {
+//
+//    private final NewsMapper newsMapper;
+//
+//    private final NewsRepository newsRepository;
+//
+//    public NewsDto createNews(NewsDto newsDto) {
+//        News news = newsMapper.toEntity(newsDto);
+//
+//        newsRepository.save(news);
+//        return newsMapper.toDto(news);
+//    }
+//
+//    public List<NewsDto> getNews() {
+//        return newsMapper.toDtoList(newsRepository.findAll());
+//    }
+//
+//    public NewsDto updateNews(Long id, NewsDto newsDto) throws Exception {
+//        News news = newsRepository.findById(id)
+//                .orElseThrow(() -> new Exception("News with ID " + id + " not found."));
+//
+//        newsMapper.updateNewsFromDto(newsDto, news);
+//
+//        newsRepository.save(news);
+//        return newsMapper.toDto(news);
+//    }
+//
+//    public void deleteNews(Long id) throws Exception {
+//        News news = newsRepository.findById(id)
+//                .orElseThrow(() -> new Exception("News with ID " + id + " not found."));
+//        newsRepository.delete(news);
+//    }
+//}
